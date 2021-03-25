96959cd87ea295d20bb83db2453052fcb7aa0185
psallandre <patrice@sallandre.com>
2021-03-25 23:26:39 +0100

    WIP

--- jetson-rf-receiver/local_build.sh
+++ jetson-rf-receiver/local_build.sh
@@ -4,7 +4,7 @@ builder="homeassistant/amd64-builder"
 docker run --rm -ti --name hassio-builder --privileged \
     -v $PWD:/src -v ~/.docker:/root/.docker -v /var/run/docker.sock:/var/run/docker.sock:ro \
     ${builder} \
-    --amd64 --armv7 --aarch64 \
+    --aarch64 \
     -t /src -i hassio-$(basename $PWD)-{arch} \
     -d ${DOCKER_REPOSITORY:-local} \
     --docker-hub-check \
